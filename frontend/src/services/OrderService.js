92a4a2cd86a13723c443da081b9664828c22b7a7
Probe1st <svintsitskiy.leha@mail.ru>
2024-05-24 18:30:57 +0500

    Update OrderService.js
    renaming

--- frontend/src/services/OrderService.js
+++ frontend/src/services/OrderService.js
@@ -1,39 +1,54 @@
-class OrderService {
-    /**
-     * get orders and return the array with them
-     * @returns {object}
-     */
-    getOrders() {
-        return;
-    }
+import { supabase } from "@/lib";
 
-    /**
-     * create the order and returns it
-     * @param {object} data
-     * @returns {object}
-     */
-    createOrder(data) {
-        return;
-    }
+class ReportsService {
+  table_name = "reports";
 
-    /**
-     * update the order and returns it 
-     * @param {object} data 
-     * @param {number} id 
-     * @returns {object}
-     */
-    updateOrder(data, id) {
-        return;
-    }
+  /**
+   * get reports and return the array with them
+   * @returns {Array}
+   */
+  async getAll() {
+    return;
+  }
 
-    /**
-     * Delete the order by id and returns it
-     * @param {number} id 
-     * @returns {object}
-     */
-    deleteOrder(id) {
-        return;
-    }
+  /**
+   * get report by id and return the array with it
+   * @param {number} id
+   * @returns {Array}
+   */
+  async getById(id) {
+    supabase.from(this.table_name);
+
+    return;
+  }
+
+  /**
+   * create the report and returns it
+   * @param {object} data
+   * @returns {object}
+   */
+  create(data) {
+    return;
+  }
+
+  /**
+   * update the report and returns it
+   * @param {object} data
+   * @param {number} id
+   * @returns {object}
+   */
+  update(data, id) {
+    return;
+  }
+
+  /**
+   * Delete the report by id and returns it
+   * @param {number} id
+   * @returns {object}
+   */
+  delete(id) {
+    return;
+  }
 }
 
-export default OrderService();
+export default ReportsService();
