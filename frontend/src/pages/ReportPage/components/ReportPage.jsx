f5ad56a92bcce8aadcd9281f3214763de690ca69
Probe1st <svintsitskiy.leha@mail.ru>
2024-05-24 18:30:25 +0500

    Update ReportPage.jsx
    redact imports and add a logic for the gettig report card

--- frontend/src/pages/ReportPage/components/ReportPage.jsx
+++ frontend/src/pages/ReportPage/components/ReportPage.jsx
@@ -1,9 +1,27 @@
-import { supabase } from "@/libs";
+import { supabase } from "@/lib";
 import ReportLayout from "../layouts/ReportLayout";
+import { useEffect, useState } from "react";
 
 export default function ReportPage() {
-  const reportId = window.location.pathname.match(/[^/]+/);
-  const reportCard = supabase.from('reports').select()
+  const reportId = window.location.pathname.match(/[^/]+$/)[0];
+  // console.log(reportId)
+
+  const [reportCard, setReportCard] = useState();
+
+  // getting reportCard
+  useEffect(() => {
+    function getReportCard() {
+      supabase
+        .from("reports")
+        .select()
+        .eq("id", reportId)
+        .then((res) => {
+          console.log(res.data[0])
+          setReportCard(res.data[0]);
+        });
+    }
+    getReportCard();
+  }, []);
 
   return <ReportLayout reportCard={reportCard} />;
 }
