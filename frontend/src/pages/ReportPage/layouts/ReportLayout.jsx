d4c35fb2ff082ce44875fdf308431587dfc5c704
Probe1st <svintsitskiy.leha@mail.ru>
2024-05-24 18:30:35 +0500

    Update ReportLayout.jsx
    add layout

--- frontend/src/pages/ReportPage/layouts/ReportLayout.jsx
+++ frontend/src/pages/ReportPage/layouts/ReportLayout.jsx
@@ -1,5 +1,48 @@
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
 
+export default function ReportLayout({ reportCard }) {
+  let cardString = "";
 
-export default function ReportLayout() {
-    
+  for (let key in reportCard) {
+    cardString += key + ": " + reportCard[key] + "\n";
+  }
+
+  console.log(cardString);
+
+  const audience = "";
+
+  return (
+    <div className="whitespace-pre-line">
+      <Card>
+        <CardHeader>
+          <CardTitle>Заявка номер {reportCard?.id}</CardTitle>
+          <CardDescription>
+            <p>Статус {reportCard?.statuses_id}</p>
+            <p>Приоритет {reportCard?.priorities_id}</p>
+          </CardDescription>
+        </CardHeader>
+        <CardContent>
+          <p>Аудитория {reportCard?.audiences_id}</p>
+          <br />
+
+          <h3>Описание</h3>
+          <p>{reportCard?.description}</p>
+        </CardContent>
+        <CardFooter>
+          <div className="whitespace-pre-line">
+            Репортер {reportCard?.reporter_user_id}
+            {reportCard?.admin_user_id && reportCard?.admin_user_id}
+            Создано
+          </div>
+        </CardFooter>
+      </Card>
+    </div>
+  );
 }
