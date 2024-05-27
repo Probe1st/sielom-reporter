b31bee2ad0f543e149ec2992d250bfb056e1a119
Probe1st <svintsitskiy.leha@mail.ru>
2024-05-27 18:36:14 +0500

    add layout and logic for report card

--- frontend/src/pages/ReportPage/components/ReportPage.jsx
+++ frontend/src/pages/ReportPage/components/ReportPage.jsx
@@ -1,27 +1,23 @@
 import { supabase } from "@/lib";
 import ReportLayout from "../layouts/ReportLayout";
 import { useEffect, useState } from "react";
+import { Report } from "@/services/Report";
 
 export default function ReportPage() {
   const reportId = window.location.pathname.match(/[^/]+$/)[0];
-  // console.log(reportId)
-
   const [reportCard, setReportCard] = useState();
 
   // getting reportCard
   useEffect(() => {
-    function getReportCard() {
-      supabase
-        .from("reports")
-        .select()
-        .eq("id", reportId)
-        .then((res) => {
-          console.log(res.data[0])
-          setReportCard(res.data[0]);
-        });
-    }
-    getReportCard();
+    Report.getById(reportId).then((data) => {
+      console.log(data[0]);
+      setReportCard(data[0]);
+    });
   }, []);
 
-  return <ReportLayout reportCard={reportCard} />;
+  return (
+    <>
+      {reportCard && <ReportLayout reportCard={reportCard} />}
+    </>
+  );
 }
--- frontend/src/pages/ReportPage/layouts/ReportLayout.jsx
+++ frontend/src/pages/ReportPage/layouts/ReportLayout.jsx
@@ -1,3 +1,4 @@
+import { Button } from "@/components/ui/button";
 import {
   Card,
   CardContent,
@@ -6,43 +7,97 @@ import {
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
+import { getDate, getMonth, getYear } from "date-fns";
 
 export default function ReportLayout({ reportCard }) {
-  let cardString = "";
+  const newLine = "\n";
 
-  for (let key in reportCard) {
-    cardString += key + ": " + reportCard[key] + "\n";
-  }
-
-  console.log(cardString);
-
-  const audience = "";
+  const {
+    id,
+    admin_user_id,
+    reporter_user_id,
+    statuses_id,
+    priorities_id,
+    audiences_id,
+    description,
+    created_at,
+    accepted_at,
+    completed_at,
+  } = reportCard;
 
   return (
-    <div className="whitespace-pre-line">
-      <Card>
+    <div className="flex flex-row justify-center items-center grow whitespace-pre-line">
+      <Card className={"max-w-[512px]"}>
         <CardHeader>
-          <CardTitle>Заявка номер {reportCard?.id}</CardTitle>
+          <CardTitle className={"text-center"}>Карточка репорта</CardTitle>
           <CardDescription>
-            <p>Статус {reportCard?.statuses_id}</p>
-            <p>Приоритет {reportCard?.priorities_id}</p>
+            Репорт: <span className="text-white">{id}</span> {newLine}
+            Статус: <span className="text-white">{statuses_id}</span> {newLine}
+            Приоритет: <span className="text-white">{priorities_id}</span>{" "}
+            {newLine}
+            Аудитория: <span className="text-white">{audiences_id}</span>
           </CardDescription>
         </CardHeader>
         <CardContent>
-          <p>Аудитория {reportCard?.audiences_id}</p>
-          <br />
-
           <h3>Описание</h3>
-          <p>{reportCard?.description}</p>
+          <p>{description}</p>
         </CardContent>
-        <CardFooter>
+        <CardFooter className="flex flex-col items-start">
+          {/* information about reporter, admin and times */}
           <div className="whitespace-pre-line">
-            Репортер {reportCard?.reporter_user_id}
-            {reportCard?.admin_user_id && reportCard?.admin_user_id}
-            Создано
+            <h3>Информация о репорте</h3>
+            Репортер: {reporter_user_id} {newLine}
+            {admin_user_id && "Админ: " + admin_user_id + newLine}
+            {newLine}
+            <div className="text-sm text-muted-foreground">
+              <Times
+                created_at={created_at}
+                accepted_at={accepted_at}
+                completed_at={completed_at}
+              />
+            </div>
+          </div>
+          {/* buttons */}
+          <div className="flex flex-row space-x-5 w-full mt-5">
+            <Button>
+              Принять
+            </Button>
+            <Button>
+              Изменить
+            </Button>
           </div>
         </CardFooter>
       </Card>
     </div>
+  );
+}
+
+function formatDateTime(timestamptz) {
+  const dateTime = new Date(timestamptz);
+  const [date, time] = [
+    getDate(dateTime) +
+      "." +
+      (getMonth(dateTime) + 1 < 10
+        ? "0" + (getMonth(dateTime) + 1)
+        : getMonth(dateTime) + 1) +
+      "." +
+      getYear(dateTime),
+    String(dateTime.getHours()) + ":" + String(dateTime.getMinutes()),
+  ];
+
+  return `${date} ${time}`;
+}
+
+function Times({ created_at, accepted_at, completed_at }) {
+  const timestampt_strings = [
+    created_at && "Создано " + formatDateTime(created_at),
+    accepted_at && "Создано " + formatDateTime(accepted_at),
+    completed_at && "Создано " + formatDateTime(completed_at),
+  ];
+
+  return timestampt_strings.reduce(
+    (accum, timestampt) =>
+      timestampt !== null ? accum + timestampt + "\n" : accum,
+    ""
   );
 }
