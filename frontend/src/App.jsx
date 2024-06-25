f5574349d9cde6b91b55e1eed4a87c082da31dfa
Probe1st <svintsitskiy.leha@mail.ru>
2024-06-25 19:08:48 +0500

    remove Header from app

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,10 @@
-import { useState } from "react";
 import "./assets/output.css";
-import Header from "./components/Header";
 import Router from "./features/routing/Router";
 
 function App() {
-  const [authorized, setAuthorized] = useState(true);
-
   return (
     <>
-      <Header />
-      <Router authorized={authorized} />
+      <Router />
     </>
   );
 }
